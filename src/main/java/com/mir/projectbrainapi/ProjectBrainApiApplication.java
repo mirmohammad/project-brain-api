43dea4f7e751b17317c0b26d7a5a9dce71550d56
Mirmohammad Saadati <mir.mohammad.saadati@gmail.com>
2020-06-30 09:48:45 -0400

    Initial Commit

---
+++ src/main/java/com/mir/projectbrainapi/ProjectBrainApiApplication.java
@@ -0,0 +1,13 @@
+package com.mir.projectbrainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBrainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBrainApiApplication.class, args);
+    }
+
+}
